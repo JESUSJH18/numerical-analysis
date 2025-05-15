7a0cdc4b49b0f871fced76626e57624b1f8f6ed8
Jesus <jesusjulo2006@gmail.com>
2025-05-15 11:06:37 +0200

    better commets to use pvi1.m

--- topic-6/pvi1.m
+++ topic-6/pvi1.m
@@ -3,6 +3,11 @@ function dy=pvi1(t,y)
 %utilizado complementariamete asi: 
 % [t,y] = Euler('pvi1', 0, 3, 1, 8)
 % plot(t,y)
-
-dy= (1-2*t)*y;
+%ejemplos: dy= (1-2*t)*y;
+%u1=y(1); ...
+% dy= [y(2);y(3) ;-6*y(1)^4];
+dy=[3*y(1)+2*y(2)-(2*t^2+1)*exp(2*t);
+4*y(1)+y(2)+(t^2+2*t-4)*exp(2*t)];
+% despues de hacer el: >> [t, y] = EulerSistemas('pvi1', 0,1, [1 1], 10)
+% puedes hacer: plot(t,y)
 end
