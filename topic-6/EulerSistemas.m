5291f402a582a985d4ccae4effd58148e059cacf
Jesus <jesusjulo2006@gmail.com>
2025-05-15 10:51:37 +0200

    EulerSistemas draft

---
+++ topic-6/EulerSistemas.m
@@ -0,0 +1,11 @@
+function [t, Y] = EulerSistemas(f, a, b, y0, N)
+% [t, Y] = EulerSistemas(f, t0, tf, y0, h)
+
+h= (b-a)/N; % Paso
+t= a:h:b; % Vector de nodos, vector fila
+y = zeros(N+1,length(y0)); % Inicializar matriz de soluciones
+y(1,:)= y0; % Condiciones iniciales
+for k=1:N
+    y(k+1,:)= y(k,:) + h*feval(f,t(k),y(k,:))'; % Metodo de Euler
+end
+end
