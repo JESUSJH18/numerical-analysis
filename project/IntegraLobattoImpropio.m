7bfa02719e7750aaa565a787b3f88bed8b88de01
Jesus <jesusjulo2006@gmail.com>
2025-05-18 23:01:39 +0200

    comment to be fixe

--- project/IntegraLobattoImpropio.m
+++ project/IntegraLobattoImpropio.m
@@ -5,6 +5,6 @@ for i=2:k
     ai=1/(2^(i));
     I=I+IntegraLobattoAB(a+ai,a+ai*2,f,n);
 end
-I=I+IntegraLobattoAB(a+0.5,b,f,n);
+I=I+IntegraLobattoAB(a+0.5,b,f,n); % esto esta mal, si b-a<0.5, a+0.5 está fuera del intervalo
 end
 
