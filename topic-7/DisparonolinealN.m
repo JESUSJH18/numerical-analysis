a1dc8a5498b4e7647e83bcea9f136b2fb466f2c2
Jesus <jesusjulo2006@gmail.com>
2025-05-29 08:40:26 +0200

    Disparo Newton draft

---
+++ topic-7/DisparonolinealN.m
@@ -0,0 +1,16 @@
+function [x,y,t,iter,incr]= DisparonolinealN(pvi,a,b,N,alfa,beta,tol,maxiter)
+    % [x,y,t,iter,incr]= DisparonolinealN(pvi,a,b,N,tol,maxit), N=Newton
+h=(b-a)/N; x=a:h:b; 
+iter=1;
+% incr=tol+1;
+t=(beta-alfa)/(b-a);
+[x,y]=ode45(pvi,x,[alfa,t,0,1]); 
+incr=abs(y(end,1)-beta);
+while incr>tol && iter<maxiter
+    t=t-(y(end,1)-beta)/(y(end,3));
+    [x,y]=ode45(pvi,x,[alfa,t,0,1]); 
+    incr=abs(y(end,1)-beta);
+    iter=iter+1;
+end
+
+end
