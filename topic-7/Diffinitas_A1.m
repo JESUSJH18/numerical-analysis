0009e0273761a288ae9c9ab6037b06541579107a
Jesus <jesusjulo2006@gmail.com>
2025-05-29 11:24:32 +0200

    fix error

--- topic-7/Diffinitas_A1.m
+++ topic-7/Diffinitas_A1.m
@@ -1,6 +1,6 @@
 function [X,solve]= Diffinitas_A1(p,q,r,a,b,alfa,beta,N)
-
-h=(b-a)/N+1; %N+2 puntos, N+1 numero de subintervalos
+    
+h=(b-a)/(N+1); %N+2 puntos, N+1 numero de subintervalos
 X=a:h:b; 
 x=X(2:end-1); X=X(:);
 px=feval(p,x);
@@ -13,7 +13,7 @@ di=-1-h/2*px(2:end);
 d=-h^2*rx;
 d(1)=d(1)+(1+(h/2)*px(1))*alfa;
 d(end)=d(end)+(1-(h/2)*px(end))*beta;
-y=crout(dp,ds,di,d); %dp= diagonal principal, ds= diagonal superior, di= diagonal inferior
+y=Crout(dp,ds,di,d); %dp= diagonal principal, ds= diagonal superior, di= diagonal inferior
 solve=[alfa;y;beta]; 
 end
 
